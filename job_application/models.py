5f9cda26d7afed6860fa5b09f6ffa9ab90afae47
ImpendingKataclysm <ktjanzen42@gmail.com>
2023-03-24 10:35:13 -0700

    Created the table in which job applicant data will be stored in the database.

--- job_application/models.py
+++ job_application/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Form(models.Model):
+    """
+    Defines the table in the database in which job applicant data is stored.
+    Defines columns for:
+    - first_name
+    - last_name
+    - email
+    - date_available
+    - occupation
+    Text fields have a maximum length of MAX_FIELD_LEN
+    """
+    MAX_FIELD_LEN = 80
+    first_name = models.CharField(max_length=MAX_FIELD_LEN)
+    last_name = models.CharField(max_length=MAX_FIELD_LEN)
+    email = models.EmailField()
+    date = models.DateField()
+    occupation = models.CharField(max_length=MAX_FIELD_LEN)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
