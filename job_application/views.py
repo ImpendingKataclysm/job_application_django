5d8e181f1f9f3c75d8e1232ab9514183cce10fcd
ImpendingKataclysm <ktjanzen42@gmail.com>
2023-03-26 16:42:04 -0700

    Email the applicant upon successful submission of the application form

--- job_application/views.py
+++ job_application/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .forms import ApplicationForm
 from .models import Form
 from django.contrib import messages
+from django.core.mail import EmailMessage
 
 
 def index(request):
@@ -14,13 +15,22 @@ def index(request):
             date = form.cleaned_data["date"]
             occupation = form.cleaned_data["occupation"]
 
+            # Store applicant data in the database
             Form.objects.create(first_name=first_name,
                                 last_name=last_name,
                                 email=email,
                                 date=date,
                                 occupation=occupation)
 
+            # Render a success message in the browser
             success_message = f"Thanks for your application, {first_name} {last_name}!"
             messages.success(request, success_message)
+
+            # Email success message to the applicant
+            message_subject = "Application submission confirmation"
+            message_body = f"Thanks for your application, {first_name} {last_name}.\n" \
+                           f"We'll be in touch soon."
+            email_message = EmailMessage(message_subject, message_body, to=[email])
+            email_message.send()
 
     return render(request, "index.html")
